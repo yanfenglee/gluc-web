bfb703cfda84187d9fe98a7cd857a89bf1af0e7e
yanfeng.li <yanfeng.li@yunqueyi.com>
2020-12-17 23:20:40 +0800

    fix

--- src/service/cgm_service.rs
+++ src/service/cgm_service.rs
@@ -26,7 +26,7 @@ impl CgmService {
         //let w = RB.new_wrapper().ge("date", rr).check()?;
         //let ret: Result<Vec<Cgm>> = RB.list_by_wrapper("", &w).await;
 
-        #[py_sql(RB, "SELECT * FROM cgm WHERE `date` > #{ts} order by `date` desc LIMIT #{cc}")]
+        #[py_sql(RB, "SELECT * FROM cgm WHERE `date` < #{ts} order by `date` desc LIMIT #{cc}")]
         fn select_entries(ts: i64, cc: i64) -> Vec<Cgm> {}
 
         let cgms = select_entries(ts, cnt).await?;
