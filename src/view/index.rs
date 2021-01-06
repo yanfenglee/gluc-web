10acf4370e075e37f2ef01e71f9f5b038169d9b9
yanfeng.li <yanfeng.li@yunqueyi.com>
2021-01-06 22:34:21 +0800

    update

--- src/view/index.rs
+++ src/view/index.rs
@@ -2,22 +2,11 @@
 use sailfish_macros;
 use sailfish::TemplateOnce;
 
-#[derive(TemplateOnce)]
+#[derive(TemplateOnce, Default)]
 #[template(path = "index.stpl")]
 pub struct Index {
     pub bg: String,
     pub delta: f32,
     pub direction: String,
     pub time: String,
-}
-
-impl Default for Index {
-    fn default() -> Self {
-        Index {
-            bg: "".to_string(),
-            delta: 0.0,
-            direction: "".to_string(),
-            time: "".to_string(),
-        }
-    }
 }
